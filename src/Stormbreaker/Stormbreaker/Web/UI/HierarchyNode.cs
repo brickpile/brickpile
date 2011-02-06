13a8ff1f7fafbfa42eaea2f2e631d3591585b214
kloojed <marcus@kloojed.com>
2011-02-06 09:46:29 +0100

    Removed references to the old DocumentRoute

--- src/Stormbreaker/Stormbreaker/Web/UI/HierarchyNode.cs
+++ src/Stormbreaker/Stormbreaker/Web/UI/HierarchyNode.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using Stormbreaker.Models;
 
 namespace Stormbreaker.Web.UI {
-    public class HierarchyNode<T> where T : IDocument
+    public class HierarchyNode<T> where T : IPageModel
     {
         public T Entity { get; set; }
         public IEnumerable<HierarchyNode<T>> ChildNodes { get; set; }
