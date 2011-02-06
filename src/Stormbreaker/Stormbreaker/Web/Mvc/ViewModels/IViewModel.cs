a8031c1a4c7237bfe3cc1412c304991807be6e29
kloojed <marcus@kloojed.com>
2011-02-06 09:50:19 +0100

    Changed the usage of IDocument to IPageModel

--- src/Stormbreaker/Stormbreaker/Web/Mvc/ViewModels/IViewModel.cs
+++ src/Stormbreaker/Stormbreaker/Web/Mvc/ViewModels/IViewModel.cs
@@ -3,9 +3,21 @@ using Stormbreaker.Models;
 using Stormbreaker.Web.UI;
 
 namespace Stormbreaker.Web.Mvc.ViewModels {
-    public interface IViewModel<out T> where T : IDocument {
-        T CurrentDocument { get; }
-        IDocument RootDocument { get; }
-        IEnumerable<HierarchyNode<IDocument>> StructureInfo { get; }
+    /// <summary>
+    /// Represents the view model
+    /// </summary>
+    public interface IViewModel<out T> {
+        /// <summary>
+        /// <see cref="DefaultViewModel{T}.CurrentModel"/>
+        /// </summary>
+        T CurrentModel { get; }
+        /// <summary>
+        /// <see cref="DefaultViewModel{T}.RootPageModel"/>
+        /// </summary>
+        IPageModel RootPageModel { get; }
+        /// <summary>
+        /// <see cref="DefaultViewModel{T}.StructureInfo"/>
+        /// </summary>
+        IEnumerable<HierarchyNode<IPageModel>> StructureInfo { get; }
     }
 }
