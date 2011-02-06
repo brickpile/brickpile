dabde9249eb298f03b40ae9ce015fcc1181dd614
kloojed <marcus@kloojed.com>
2011-02-06 09:54:33 +0100

    Removed references to IDocument and added documentation

--- src/Stormbreaker/Stormbreaker/Repositories/IRepository.cs
+++ src/Stormbreaker/Stormbreaker/Repositories/IRepository.cs
@@ -1,19 +1,33 @@
-using System.Collections.Generic;
 using Stormbreaker.Models;
 
 namespace Stormbreaker.Repositories {
     /// <summary>
-    /// The IRepository interface 
+    /// Represents a repository 
     /// </summary>
     public interface IRepository {
-
-        IDocument[] GetChildren<T>(IDocument entity);
-
-        T LoadDocumentBySlug<T>(string slug);
+        /// <summary>
+        /// <see cref="PageRepository.GetChildren{T}<>" />
+        /// </summary>
+        IPageModel[] GetChildren<T>(IPageModel entity);
+        /// <summary>
+        /// <see cref="PageRepository.GetPageBySlug{T}<>" />
+        /// </summary>
+        T GetPageBySlug<T>(string slug);
+        /// <summary>
+        /// <see cref="PageRepository.Load{T}<>" />
+        /// </summary>
         T Load<T>(string id);
-
-        void Store(IDocument entity);
-        void Delete(IDocument entity);
+        /// <summary>
+        /// <see cref="PageRepository.Store" />
+        /// </summary>
+        void Store(IPageModel entity);
+        /// <summary>
+        /// <see cref="PageRepository.Delete" />
+        /// </summary>
+        void Delete(IPageModel entity);
+        /// <summary>
+        /// <see cref="PageRepository.SaveChanges" />
+        /// </summary>
         void SaveChanges();
     }
 }
