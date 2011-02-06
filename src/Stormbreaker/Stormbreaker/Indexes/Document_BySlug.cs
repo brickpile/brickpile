04d8bf70fd7c696950ed12420043be0794802e16
kloojed <marcus@kloojed.com>
2011-02-06 10:08:46 +0100

    Removed references to IDocument and added MetaData as a part of the document

--- src/Stormbreaker/Stormbreaker/Indexes/Document_BySlug.cs
+++ src/Stormbreaker/Stormbreaker/Indexes/Document_BySlug.cs
@@ -4,16 +4,25 @@ using Raven.Database.Indexing;
 using Stormbreaker.Models;
 
 namespace Stormbreaker.Indexes {
+    /// <summary>
+    /// Used for registrations of an index in RavenDB
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
     public class Document_BySlug : AbstractIndexCreationTask
     {
+        /// <summary>
+        /// Creates the index definition for getting a page model by slug
+        /// </summary>
+        /// <returns></returns>
         public override IndexDefinition CreateIndexDefinition()
         {
-            return new IndexDefinition<IDocument>
+            return new IndexDefinition<IPageModel>
             {
                 Map = documents => from document in documents
-                                select new { document.Slug }
+                                   select new { document.MetaData.Slug }
             }
             .ToIndexDefinition(DocumentStore.Conventions);
-        }        
+        }
     }
 }
