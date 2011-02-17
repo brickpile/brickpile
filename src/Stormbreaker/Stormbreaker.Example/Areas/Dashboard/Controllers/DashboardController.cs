be77b938762ba2115a9b89b9a9fef0f281e4ea47
kloojed <marcus@kloojed.com>
2011-02-17 17:35:10 +0100

    Removed restsharp dependencies

--- src/Stormbreaker/Stormbreaker.Example/Areas/Dashboard/Controllers/DashboardController.cs
+++ src/Stormbreaker/Stormbreaker.Example/Areas/Dashboard/Controllers/DashboardController.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Web;
-using System.Web.Hosting;
 using System.Web.Mvc;
-using RestSharp;
-using RestSharp.Deserializers;
 using Stormbreaker.Repositories;
 using Stormbreaker.Web.Hosting;
 
