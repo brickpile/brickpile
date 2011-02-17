17c4c115cc72f584cb41da350171cd60057ec5fe
kloojed <marcus@kloojed.com>
2011-02-17 17:32:35 +0100

    Removed restsharp dependency

--- src/Stormbreaker/Stormbreaker/Web/Hosting/DropboxVirtualPathProvider.cs
+++ src/Stormbreaker/Stormbreaker/Web/Hosting/DropboxVirtualPathProvider.cs
@@ -1,5 +1,4 @@
 using System.Web.Hosting;
-using RestSharp;
 
 namespace Stormbreaker.Web.Hosting {
     public class DropboxVirtualPathProvider : VirtualPathProvider
@@ -10,29 +9,29 @@ namespace Stormbreaker.Web.Hosting {
         private readonly DropboxDirectory _root;
 
         public override VirtualFile GetFile(string virtualPath) {
-            _userLogin = Login("marcus@meridium.se", "pkrMum");
+            //_userLogin = Login("marcus@meridium.se", "pkrMum");
             
             return base.GetFile(virtualPath);
         }
 
-        public UserLogin Login(string email, string password)
-        {
-            var restClient = new RestClient("https://api.getdropbox.com");
-            var request = new RestRequest(Method.GET);
-            request.Resource = "{version}/token";
-            request.AddParameter("version", _version, ParameterType.UrlSegment);
+        //public UserLogin Login(string email, string password)
+        //{
+            //var restClient = new RestClient("https://api.getdropbox.com");
+            //var request = new RestRequest(Method.GET);
+            //request.Resource = "{version}/token";
+            //request.AddParameter("version", _version, ParameterType.UrlSegment);
 
-            request.AddParameter("oauth_consumer_key", _apiKey);
+            //request.AddParameter("oauth_consumer_key", _apiKey);
 
-            request.AddParameter("email", email);
-            request.AddParameter("password", password);
+            //request.AddParameter("email", email);
+            //request.AddParameter("password", password);
 
-            var response = restClient.Execute<UserLogin>(request);
+            //var response = restClient.Execute<UserLogin>(request);
 
-            _userLogin = response.Data;
+            //_userLogin = response.Data;
 
-            return response.Data;
-        }
+            //return response.Data;
+        //}
 
         public override bool FileExists(string virtualPath)
         {
