32a3063a50da078addb1fc99abe01a63d88f9e46
kloojed <marcus@kloojed.com>
2011-02-21 21:42:50 +0100

    Added page selection model to the dashboard model

--- src/Stormbreaker/Stormbreaker.Example/Areas/Dashboard/Controllers/ContentController.cs
+++ src/Stormbreaker/Stormbreaker.Example/Areas/Dashboard/Controllers/ContentController.cs
@@ -2,28 +2,34 @@
 using System.Web.Mvc;
 using Dashboard.Models;
 using Dashboard.Web.Mvc.ViewModels;
+using Stormbreaker.Configuration;
 using Stormbreaker.Exceptions;
 using Stormbreaker.Models;
 using Stormbreaker.Repositories;
+using Stormbreaker.Web.UI;
 
 namespace Dashboard.Controllers {
-
     public class ContentController : Controller {
-        private readonly PageRepository _repository;
+        private readonly IPageRepository _repository;
+        private readonly IStructureInfo _structureInfo;
+        private readonly IConfiguration _configuration;
         /// <summary>
         /// Default action
         /// </summary>
-        /// <returns>Redirects to the Edit action with the home page loaded</returns>
+        /// <returns>
+        /// Redirects to the Edit action with the home page loaded
+        /// </returns>
         public ActionResult Index() {
-            return RedirectToAction("edit", new { model = _repository.Load<dynamic>("pages/1")}); 
+            return RedirectToAction("edit", new { model = _repository.Load<dynamic>(_configuration.HomePageId)}); 
         }
         /// <summary>
         /// Responsible for providing the Edit view with data from the current page
         /// </summary>
+        /// <param name="model">The model.</param>
         /// <returns></returns>
         public ActionResult Edit(dynamic model)
         {
-            var viewModel = new DashboardViewModel(model, _repository);
+            var viewModel = new DashboardViewModel(model, _structureInfo,_repository);
             return View(viewModel);
         }
         /// <summary>
@@ -43,21 +49,22 @@ namespace Dashboard.Controllers {
 
                 return RedirectToAction("edit", new { model });
             }
-            return View("edit", new DashboardViewModel(model, _repository));
+            return View("edit", new DashboardViewModel(model, _structureInfo,_repository));
         }
         /// <summary>
         /// Responsible for providing the add page view with data
         /// </summary>
+        /// <param name="model">The model.</param>
         /// <returns></returns>
         public ActionResult Add(dynamic model)
         {
-            return View("add", new DashboardViewModel(model, _repository));
+            return View("add", new DashboardViewModel(model, _structureInfo,_repository));
         }
         /// <summary>
         /// Responsible for creating a new page based on the selected page model
         /// </summary>
-        /// <param name="newPageModel"></param>
-        /// /// <param name="model"></param>
+        /// <param name="newPageModel">The new page model.</param>
+        /// <param name="model">The model.</param>
         /// <returns></returns>
         public ActionResult Create([Bind(Prefix = "NewPageModel")] NewPageModel newPageModel, dynamic model)
         {
@@ -80,8 +87,13 @@ namespace Dashboard.Controllers {
                 return RedirectToAction("edit", new { model = page });
             }
 
-            return View("add", new DashboardViewModel(newPageModel, _repository));
+            return View("add", new DashboardViewModel(newPageModel, _structureInfo,_repository));
         }
+        /// <summary>
+        /// Deletes the specified model.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        /// <returns></returns>
         public ActionResult Delete(dynamic model)
         {
             _repository.Delete(model);
@@ -91,10 +103,14 @@ namespace Dashboard.Controllers {
         /// <summary>
         /// Initializes a new instance of the <b>PagesController</b> class.
         /// </summary>
-        /// <param name="repository"></param>
-        public ContentController(PageRepository repository)
+        /// <param name="repository">The repository.</param>
+        /// <param name="structureInfo">The structure info.</param>
+        /// <param name="configuration">The configuration.</param>
+        public ContentController(IPageRepository repository, IStructureInfo structureInfo, IConfiguration configuration)
         {
             _repository = repository;
+            _structureInfo = structureInfo;
+            _configuration = configuration;
         }
     }
 }
