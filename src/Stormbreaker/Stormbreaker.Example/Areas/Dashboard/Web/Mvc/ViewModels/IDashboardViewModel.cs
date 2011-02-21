a6a58c5516fce633725c61a5664d1fe82f08ea4d
kloojed <marcus@kloojed.com>
2011-02-21 21:41:26 +0100

    Added page selection model to the dashboard model

--- src/Stormbreaker/Stormbreaker.Example/Areas/Dashboard/Web/Mvc/ViewModels/IDashboardViewModel.cs
+++ src/Stormbreaker/Stormbreaker.Example/Areas/Dashboard/Web/Mvc/ViewModels/IDashboardViewModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Dashboard.Models;
 using Stormbreaker.Models;
 using Stormbreaker.Web.UI;
@@ -6,6 +7,7 @@ namespace Dashboard.Web.Mvc.ViewModels {
     public interface IDashboardViewModel {
         IPageModel CurrentModel { get; }
         IStructureInfo StructureInfo { get; }
+        IList<IPageModel> PageSelectionModel { get; }
         NewPageModel NewPageModel { get; }
     }
 }
