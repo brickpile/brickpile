1cf610ae74b7f04da6dada83e8c76404b11016d8
kloojed <marcus@kloojed.com>
2011-09-12 20:13:05 +0200

    Changed ccnet configuration

--- src/BrickPile/BrickPile.Sample/Controllers/HomeController.cs
+++ src/BrickPile/BrickPile.Sample/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using BrickPile.Core.Infrastructure.Common;
+using BrickPile.Core.Repositories;
 using BrickPile.Domain.Models;
 using BrickPile.UI;
 using BrickPile.UI.Web.ViewModels;
@@ -13,23 +14,25 @@ namespace BrickPile.Sample.Controllers
         private readonly IPageModel _model;
         private readonly IStructureInfo _structureInfo;
         private readonly IDocumentSession _session;
+        private readonly IPageRepository _repository;
         public ActionResult Index()
         {
             return View(new DefaultViewModel<IPageModel>(_model,_structureInfo));
         }
         public ActionResult Foo() {
 
-            var children = _session.Query<IPageModel>()
-                .GetChildren(_model)
-                .Where(x => x.Metadata.PublishedStatus);
+            var children = _repository.GetChildren(_model);
+            
+            children = _session.Query<IPageModel>().Where(x=> x.Parent.Id == _model.Id).Where(model => model.Metadata.PublishedStatus);
 
             return Json(children,JsonRequestBehavior.AllowGet);
 
         }
-        public HomeController(IPageModel model, IStructureInfo structureInfo, IDocumentSession session) {
+        public HomeController(IPageModel model, IStructureInfo structureInfo, IDocumentSession session, IPageRepository repository) {
             _model = model;
             _structureInfo = structureInfo;
             _session = session;
+            _repository = repository;
         }
     }
 }
