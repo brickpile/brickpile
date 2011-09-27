83d347c2a73ce2e7c2ece160366931496f9ed718
kloojed <marcus@kloojed.com>
2011-09-27 10:09:55 +0200

    Upgraded RavenDB

--- src/BrickPile/BrickPile.Sample/Controllers/PageController.cs
+++ src/BrickPile/BrickPile.Sample/Controllers/PageController.cs
@@ -5,7 +5,6 @@ using BrickPile.Domain.Models;
 using BrickPile.Sample.Models;
 using BrickPile.UI;
 using BrickPile.UI.Web.ViewModels;
-using Raven.Client;
 
 namespace BrickPile.Sample.Controllers
 {
@@ -13,18 +12,13 @@ namespace BrickPile.Sample.Controllers
     {
         private readonly IPageModel _model;
         private readonly IStructureInfo _structureInfo;
-        private readonly IDocumentSession _session;
         public ActionResult Index() {
-
-            var children = _session.Query<IPageModel>().WherePageIsPublished();
-
             return View(new DefaultViewModel<Page>(_model as Page,_structureInfo));
         }
         
-        public PageController(IPageModel model, IStructureInfo structureInfo,IDocumentSession session) {
+        public PageController(IPageModel model, IStructureInfo structureInfo) {
             _model = model;
             _structureInfo = structureInfo;
-            _session = session;
         }
     }
 }
