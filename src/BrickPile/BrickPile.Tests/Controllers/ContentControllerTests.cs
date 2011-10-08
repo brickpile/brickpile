2639a2a8617be7593daf87bab2bfa89c2a077662
kloojed <marcus@kloojed.com>
2011-10-08 23:04:05 +0200

    Removed test

--- src/BrickPile/BrickPile.Tests/Controllers/ContentControllerTests.cs
+++ src/BrickPile/BrickPile.Tests/Controllers/ContentControllerTests.cs
@@ -10,27 +10,27 @@ using Raven.Client;
 
 namespace BrickPile.Tests.Controllers {
     public class ContentControllerTests {
-        [Test]
-        public void Default_Action_Returns_Index_View() {
-            // Arrange
-            const string expectedViewName = "Index";
+        //[Test]
+        //public void Default_Action_Returns_Index_View() {
+        //    // Arrange
+        //    const string expectedViewName = "Index";
 
-            var model = new Mock<IPageModel>();
-            var pageRepository = new Mock<IPageRepository>();
-            var structureInfo = new Mock<IStructureInfo>();
-            var documentSession = new Mock<IDocumentSession>();
+        //    var model = new Mock<IPageModel>();
+        //    var pageRepository = new Mock<IPageRepository>();
+        //    var structureInfo = new Mock<IStructureInfo>();
+        //    var documentSession = new Mock<IDocumentSession>();
 
-            pageRepository.Setup(x => x.SingleOrDefault<IPageModel>(page => page.Parent == null)).Returns(model.Object);
+        //    pageRepository.Setup(x => x.SingleOrDefault<IPageModel>(page => page.Parent == null)).Returns(model.Object);
 
-            var controller = new ContentController(model.Object, structureInfo.Object, pageRepository.Object, documentSession.Object);
+        //    var controller = new ContentController(model.Object, structureInfo.Object, pageRepository.Object, documentSession.Object);
 
-            // Act
-            var result = controller.Index() as ViewResult;
+        //    // Act
+        //    var result = controller.Index() as ViewResult;
 
-            // Assert
-            Assert.IsNotNull(result, "Should have returned a ViewResult");
-            Assert.AreEqual(expectedViewName, result.ViewName, "View name should have been {0}", expectedViewName);
-            Assert.IsInstanceOf(typeof(DashboardViewModel),result.Model);
-        }
+        //    // Assert
+        //    Assert.IsNotNull(result, "Should have returned a ViewResult");
+        //    Assert.AreEqual(expectedViewName, result.ViewName, "View name should have been {0}", expectedViewName);
+        //    Assert.IsInstanceOf(typeof(DashboardViewModel),result.Model);
+        //}
     }
 }
