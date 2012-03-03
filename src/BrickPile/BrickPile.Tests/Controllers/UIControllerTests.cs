761007e5c6dda05f31fbff9ee065dcb68b39f0c3
kloojed <marcus@kloojed.com>
2012-03-03 16:08:45 +0100

    Removed backbone and underscore

---
+++ src/BrickPile/BrickPile.Tests/Controllers/UIControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using BrickPile.UI.Controllers;
+using NUnit.Framework;
+
+namespace BrickPile.Tests.Controllers {
+    class UIControllerTests {
+        [Test]
+        public void Default_Action_Returns_Index_View() {
+            // Arrange
+            const string expectedViewName = "Index";
+
+            var controller = new UIController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, "Should have returned a ViewResult");
+            Assert.AreEqual(expectedViewName, result.ViewName, "View name should have been {0}", expectedViewName);
+            //Assert.IsInstanceOf(typeof(DashboardViewModel), result.Model);
+        }
+    }
+}
