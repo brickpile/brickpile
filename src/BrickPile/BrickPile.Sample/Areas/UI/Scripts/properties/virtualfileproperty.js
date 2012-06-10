4130cdd68f187806905deef7b11f4d8d211ce073
Marcus Lindblom <marcus@kloojed.com>
2012-06-10 13:55:23 +0200

    Fixed issue with more then one virtual file selector on a page

--- src/BrickPile/BrickPile.Sample/Areas/UI/Scripts/properties/virtualfileproperty.js
+++ src/BrickPile/BrickPile.Sample/Areas/UI/Scripts/properties/virtualfileproperty.js
@@ -22,27 +22,31 @@ THE SOFTWARE. */
 // ----------
 
 var VirtualFile = Backbone.Model.extend({
-
     Url: null,
+
     Etag: null,
+
     LocalPath: null,
+
     IsDirectory: null,
+
     Name: null,
+    
     VirtualPath: null,
 
     initialize: function () { }
-
+    
 });
 
 // VirtualFile Collection
 // ----------------------
 
 var VirtualFileCollection = Backbone.Collection.extend({
-
+    
     url: '/assets',
 
     model: VirtualFile
-
+    
 });
 
 // VirtualFile property modal view
@@ -50,14 +54,16 @@ var VirtualFileCollection = Backbone.Collection.extend({
 
 var VirtualFileSelectorModalView = Backbone.View.extend({
 
+    currentSelectedModel: null,
+
     events: {
-        'click a.close': 'cancel',
-        'click a.cancel': 'cancel',
-        'click a.select': 'select'
+        'click a.close': 'cancelAndClose',
+        'click a.cancel': 'cancelAndClose',
+        'click a.select': 'selectAndClose'
     },
 
     // Cancel and close the dialog
-    cancel: function () {
+    cancelAndClose: function () {
         $(this.el).find('.modal').fadeOut('fast', function () {
             $(this).remove();
             $('.modal-backdrop').remove();
@@ -65,17 +71,39 @@ var VirtualFileSelectorModalView = Backbone.View.extend({
         return false;
     },
 
-    select: function () {
+    selectAndClose: function () {
+
+        var self = this;
+        this.$el.find('input:hidden.url').val(currentSelectedModel.get('Url'));
+        this.$el.find('input:hidden.virtualPath').val(currentSelectedModel.get('VirtualPath'));
+
+        $.ajax({
+            url: '/assets/getthumbnailurl',
+            data: { path: currentSelectedModel.get('VirtualPath') },
+            success: function (data) {
+                self.$el.find('.centerbox img').attr('src', data);
+            }
+        });
+
         $(this.el).find('.modal').fadeOut('fast', function () {
             $(this).remove();
             $('.modal-backdrop').remove();
         });
         return false;
     },
-    
+
+    select: function (model) {
+        currentSelectedModel = model;
+    },
+
     initialize: function () {
         this.collection.bind("reset", this.render, this);
         this.template = _.template($('#view-template-virtual-file-dialog').html());
+
+        // Shorthand for the application namespace
+        var app = brickpile.app;
+        // bind to the select event
+        app.bind('select', this.select, this);
     },
 
     render: function () {
@@ -86,7 +114,12 @@ var VirtualFileSelectorModalView = Backbone.View.extend({
         // Find the dialog body and append the thumbnails
         var $ul = $(this.el).find('.modal-body ul');
         this.collection.each(function (virtualFile) {
-            var fileview = new VirtualFileView({ model: virtualFile });
+            var fileview = new VirtualFileView({
+                model: virtualFile,
+                inputUrl: this.$el.find('input:hidden.url'),
+                inputVirtualUrl: this.$el.find('input:hidden.virtualUrl'),
+                thumbnail: this.$el.find('.centerbox img')
+            });
             var $li = fileview.render().$el;
             $ul.append($li);
         }, this);
@@ -100,7 +133,7 @@ var VirtualFileSelectorModalView = Backbone.View.extend({
         });
         return this;
     }
-    
+
 });
 
 // VirtualFile property view
@@ -119,18 +152,7 @@ var VirtualFilePropertyView = Backbone.View.extend({
         coll.fetch();
     },
 
-    selected: function (model) {
-        console.log('Event fires with ' + this.$el);
-        this.$el.find('input:hidden.url').val(model.get('Url'));
-        this.$el.find('input:hidden.virtualUrl').val(model.get('VirtualPath'));
-    },
-
-    initialize: function () {
-        // Shorthand for the application namespace
-        var app = brickpile.app;
-        // bind to the selected event
-        app.bind('selected', this.selected, this);
-    },
+    initialize: function () {},
 
     render: function () { }
 
@@ -140,7 +162,7 @@ var VirtualFilePropertyView = Backbone.View.extend({
 // -------------------------
 
 var VirtualFileView = Backbone.View.extend({
-
+    
     template: _.template($('#view-template-virtual-file').html()),
 
     events: {
@@ -148,18 +170,23 @@ var VirtualFileView = Backbone.View.extend({
     },
 
     select: function () {
+        
         this.$el.parents('ul').children('li').removeClass('selected');
         this.$el.addClass('selected');
+
         // Shorthand for the application namespace
         var app = brickpile.app;
         // Trigger the selected event
-        app.trigger('selected', this.model);
+        app.trigger('select', this.model);
+
     },
+
+    initialize: function () { },
 
     render: function () {
         var html = this.template(this.model.toJSON());
         this.setElement($(html));
         return this;
     }
-    
+
 });
