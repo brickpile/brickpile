e1b959edeceee8c9adbf931d4998231ebf70ae77
Marcus Lindblom <marcus@kloojed.com>
2012-08-15 20:06:52 +0200

    Moved filters

---
+++ src/BrickPile/BrickPile.Sample/Areas/UI/App_Start/FilterConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace BrickPile.UI2 {
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FilterConfig {
+        /// <summary>
+        /// Registers the global filters.
+        /// </summary>
+        /// <param name="filters">The filters.</param>
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
