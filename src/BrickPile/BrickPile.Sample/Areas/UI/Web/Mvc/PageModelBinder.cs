c4d8584e83e4fd46c90b764ce252f53ce669cef7
Marcus Lindblom <marcus@kloojed.com>
2012-09-27 20:06:43 +0200

    Added missing md file in documentation

--- src/BrickPile/BrickPile.Sample/Areas/UI/Web/Mvc/PageModelBinder.cs
+++ src/BrickPile/BrickPile.Sample/Areas/UI/Web/Mvc/PageModelBinder.cs
@@ -25,7 +25,7 @@ namespace BrickPile.UI.Web.Mvc {
 
                     // We can only handle
                     if (attribute.GetType().BaseType == typeof(ValidationAttribute) && property.PropertyType == typeof(PageReference)) {
-                        
+
                         var pageReference = bindingContext.ModelType.GetProperty(property.Name).GetValue(bindingContext.Model, null) as PageReference;
 
                         Type attrType = attribute.GetType();
@@ -44,24 +44,5 @@ namespace BrickPile.UI.Web.Mvc {
 
             base.OnModelUpdated(controllerContext, bindingContext);
         }
-
-
-
-        /// <summary>
-        /// Returns the value of a property using the specified controller context, binding context, property descriptor, and property binder.
-        /// </summary>
-        /// <param name="controllerContext">The context within which the controller operates. The context information includes the controller, HTTP content, request context, and route data.</param>
-        /// <param name="bindingContext">The context within which the model is bound. The context includes information such as the model object, model name, model type, property filter, and value provider.</param>
-        /// <param name="propertyDescriptor">The descriptor for the property to access. The descriptor provides information such as the component type, property type, and property value. It also provides methods to get or set the property value.</param>
-        /// <param name="propertyBinder">An object that provides a way to bind the property.</param>
-        /// <returns>
-        /// An object that represents the property value.
-        /// </returns>
-        //protected override object GetPropertyValue(ControllerContext controllerContext, ModelBindingContext bindingContext, System.ComponentModel.PropertyDescriptor propertyDescriptor, IModelBinder propertyBinder) {
-        //    if (propertyDescriptor.PropertyType == typeof(HtmlString)) {
-        //        return new HtmlString(bindingContext.ValueProvider.GetValue(bindingContext.ModelName).AttemptedValue);
-        //    }
-        //    return base.GetPropertyValue(controllerContext, bindingContext, propertyDescriptor, propertyBinder);
-        //}
     }
 }
--- src/BrickPile/BrickPile.Sample/Models/Page.cs
+++ src/BrickPile/BrickPile.Sample/Models/Page.cs
@@ -18,7 +18,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE. */
 
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Helpers;
 using BrickPile.Domain;
 using BrickPile.Domain.Models;
 using BrickPile.Sample.Controllers;
