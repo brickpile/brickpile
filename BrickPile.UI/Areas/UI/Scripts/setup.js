700804565f2a6381f4bbbf3c032789cde902e40f
Marcus Lindblom <marcus@kloojed.com>
2013-02-13 23:20:13 +0100

    Fixed paths to css and javascript

---
+++ BrickPile.UI/Areas/UI/Scripts/setup.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    // Hide error labels when clicked
+    $('.field-validation-error').live('click', function () {
+        $(this).fadeOut('normal', function () {
+            $(this).remove();
+        });
+    });
+
+    // Disable the submit button until all fields is filled
+    var $input = $('form fieldset input'), $register = $('input:submit');
+    $register.attr('disabled', true);
+    $register.closest('span').addClass('disabled');
+    $input.keyup(function () {
+        var trigger = false;
+        $input.each(function () {
+            if (!$(this).val()) {
+                trigger = true;
+            }
+        });
+        trigger ? $register.attr('disabled', true).closest('span').addClass('disabled') : $register.removeAttr('disabled').closest('span').removeClass('disabled');
+    });
+
+});
