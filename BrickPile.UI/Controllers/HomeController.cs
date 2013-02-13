5d12403b4cdbe5c3129ed2e7356b9ad674fc4149
Marcus Lindblom <marcus@kloojed.com>
2013-02-13 22:55:21 +0100

    Moved UI project to the root of the application. fixes #101

---
+++ BrickPile.UI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrickPile.UI.Models;
+
+namespace BrickPile.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index(Home currentContent)
+        {
+            return View(currentContent);
+        }
+
+    }
+}
---
+++ BrickPile.UI/Models/Home.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BrickPile.Domain;
+using BrickPile.Domain.Models;
+using BrickPile.UI.Controllers;
+using BrickPile.UI.Web.ViewModels;
+
+namespace BrickPile.UI.Models {
+    [ContentType(Name = "Home", ControllerType = typeof(HomeController))]
+    public class Home : IContent {
+        [ScaffoldColumn(false)]
+        public string Id { get; set; }
+
+        public Image Image { get; set; }
+
+        [DataType(DataType.Html)]
+        public string Html { get; set; }
+    }
+}
