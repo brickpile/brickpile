fb98ba6f856dd0de535da08cbb30c99df84a1edd
Marcus Lindblom <marcus@meridium.se>
2013-08-01 17:26:31 +0200

    Removed unnecessary list

--- BrickPile.UI/Web/Mvc/ContentTypeMetadataValidatorProvider.cs
+++ BrickPile.UI/Web/Mvc/ContentTypeMetadataValidatorProvider.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web.Mvc;
 using BrickPile.Core.DataAnnotations;
 
@@ -19,11 +18,10 @@ namespace BrickPile.UI.Web.Mvc {
         /// A list of validators.
         /// </returns>
         protected override IEnumerable<ModelValidator> GetValidators(ModelMetadata metadata, ControllerContext context, IEnumerable<Attribute> attributes) {
-            List<ModelValidator> validators = base.GetValidators(metadata, context, attributes).ToList();
             if (typeof(IValidatableProperty).IsAssignableFrom(metadata.ModelType)) {
-                validators.Add(new ValidatablePropertyAdapter(metadata, context));
+                yield return new ValidatablePropertyAdapter(metadata, context);
             }
-            return validators.AsEnumerable();
+            base.GetValidators(metadata, context, attributes);
         }
     }
 }
