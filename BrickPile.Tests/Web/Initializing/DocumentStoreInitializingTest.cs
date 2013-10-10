f50bf64eb891ff7b2770c109c78ba04aa1609409
Niklas Emevi <niklasm@meridium.se>
2013-10-10 13:26:43 +0200

    Changed how Brickpile initializes its documentstore

---
+++ BrickPile.Tests/Web/Initializing/DocumentStoreInitializingTest.cs
@@ -0,0 +1,32 @@
+﻿using BrickPile.Core.Infrastructure.Listeners;
+using NUnit.Framework;
+using System.Linq;
+using Raven.Client;
+using Raven.Client.Embedded;
+using StructureMap;
+using WebActivatorEx;
+
+namespace BrickPile.Tests.Web.Initializing {
+    public class DocumentStoreInitializingTest {
+        private IDocumentStore _store;
+        [SetUp]
+        public void Setup() {
+            _store = new EmbeddableDocumentStore {
+                RunInMemory = true
+            };
+        }
+        [Test]
+        public void Can_Replace_Default_DocumentStore()
+        {
+            ObjectFactory.Initialize(x => x.For<IDocumentStore>().Use(_store));
+            ActivationManager.RunPreStartMethods();
+
+            var documentStoreFromStructureMap = ObjectFactory.GetInstance<IDocumentStore>();
+            //The RunPreStartMethods Initializer registeres this StoreListener. Make sure its registered in this EmbeddableDocumentStore
+            Assert.True(((EmbeddableDocumentStore)_store).RegisteredStoreListeners.Any(x => x is StoreListener));
+
+            Assert.AreSame(_store,documentStoreFromStructureMap);
+        }
+
+    }
+}
--- BrickPile.UI/App_Start/RavenConfig.cs
+++ BrickPile.UI/App_Start/RavenConfig.cs
@@ -2,9 +2,11 @@
 using BrickPile.Core.Infrastructure.Indexes;
 using BrickPile.Core.Infrastructure.Listeners;
 using BrickPile.Domain.Models;
+using Raven.Client;
 using Raven.Client.Document;
 using Raven.Client.Embedded;
 using Raven.Client.Indexes;
+using StructureMap;
 
 namespace BrickPile.UI {
     /// <summary>
@@ -15,14 +17,20 @@ namespace BrickPile.UI {
         /// Initializes the raven.
         /// </summary>
         /// <returns></returns>
-        public static DocumentStore InitializeRaven() {
-            var store = new EmbeddableDocumentStore
-            {
-                DataDirectory = "~/App_Data/Raven"
-            };
-            if (ConfigurationManager.ConnectionStrings["RavenDB"] != null)
-            {
-                store.ConnectionStringName = "RavenDB";
+        public static DocumentStore InitializeRaven()
+        {
+
+            DocumentStore store;
+            if (ObjectFactory.Model.HasDefaultImplementationFor<IDocumentStore>()) {
+                store = (DocumentStore)ObjectFactory.GetInstance(typeof(IDocumentStore));
+            }
+            else {
+                store = new EmbeddableDocumentStore {
+                                    DataDirectory = "~/App_Data/Raven"
+                                };
+                if (ConfigurationManager.ConnectionStrings["RavenDB"] != null) {
+                    store.ConnectionStringName = "RavenDB";
+                }
             }
             store.RegisterListener(new StoreListener());
             store.Initialize();
