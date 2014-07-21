fb225c22ddf4e36de2ceb5024ed733bd6d732cb1
Marcus Lindblom <marcus@meridium.se>
2014-07-21 12:30:40 +0200

    Rename misspelled class - BrickPile.Core/Graph/ContentTypeRegistrationConvetion.cs -> BrickPile.Core/Graph/ContentTypeRegistrationConvention.cs

--- BrickPile.Core/Graph/ContentTypeRegistrationConvention.cs
+++ BrickPile.Core/Graph/ContentTypeRegistrationConvention.cs
@@ -1,51 +1,55 @@
-using System;
+﻿using System;
 using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
-using BrickPile.Domain.Models;
+using BrickPile.Core.Routing;
 using StructureMap.Configuration.DSL;
 using StructureMap.Graph;
 
-namespace BrickPile.Core.Graph {
-    /// <summary>
-    /// 
-    /// </summary>
-    public class ContentTypeRegistrationConvetion : IRegistrationConvention {
+namespace BrickPile.Core.Graph
+{
+    public class ContentTypeRegistrationConvention : IRegistrationConvention
+    {
         /// <summary>
-        /// 
+        /// The register method
         /// </summary>
-        public static readonly MethodInfo RegisterMethod = typeof(ContentTypeRegistrationConvetion)
+        public static readonly MethodInfo RegisterMethod = typeof(ContentTypeRegistrationConvention)
             .GetMethod("Register", BindingFlags.NonPublic | BindingFlags.Static)
             .GetGenericMethodDefinition();
+
         /// <summary>
         /// Processes the specified type.
         /// </summary>
         /// <param name="type">The type.</param>
         /// <param name="registry">The registry.</param>
-        public void Process(Type type, Registry registry) {
-            if (typeof(IPage).IsAssignableFrom(type)) {
-                var specificRegisterMethod = RegisterMethod.MakeGenericMethod(new[] { type });
-                specificRegisterMethod.Invoke(null, new object[] { registry });
-            }
+        public void Process(Type type, Registry registry)
+        {
+            if (!typeof (IPage).IsAssignableFrom(type)) return;
+            var specificRegisterMethod = RegisterMethod.MakeGenericMethod(new[] { type });
+            specificRegisterMethod.Invoke(null, new object[] { registry });
         }
+
         /// <summary>
         /// Registers the specified registry.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="registry">The registry.</param>
-        static private void Register<T>(Registry registry) where T : IPage {
+        static private void Register<T>(Registry registry) where T : IPage
+        {
             registry.For<T>().UseSpecial(y => y.ConstructedBy(r => GetCurrentPage<T>()));
         }
+
         /// <summary>
         /// Gets the current content.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        static private T GetCurrentPage<T>() where T : IPage {
+        static private T GetCurrentPage<T>() where T : IPage
+        {
             var handler = (MvcHandler)HttpContext.Current.Handler;
             if (handler == null)
                 return default(T);
-            return (T)handler.RequestContext.RouteData.Values["currentPage"];
+            return (T)handler.RequestContext.RouteData.Values[PageRoute.CurrentPageKey];
         }
     }
 }
