e51444a99d11291b35de48ad84501f8933777a76
Marcus Lindblom <marcus@meridium.se>
2014-07-24 18:06:08 +0200

    Add helper extension methods for route data     - Added RouteData.GetCurrentPage<T>()     - Added RouteData.GetNavigationContext()

---
+++ BrickPile.Core/Extensions/RouteDataExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Routing;
+using BrickPile.Core.Routing;
+
+namespace BrickPile.Core.Extensions
+{
+    public static class RouteDataExtensions
+    {
+        public static T GetCurrentPage<T>(this RouteData data)
+        {
+            return (T)data.Values[PageRoute.CurrentPageKey];
+        }
+
+        public static NavigationContext GetNavigationContext(this RouteData data) {
+            return new NavigationContext(HttpContext.Current.Request.RequestContext);            
+        }
+
+    }
+}
