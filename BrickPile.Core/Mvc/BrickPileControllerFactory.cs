7b8f1bb72350fdc4ae0720451576d55b8d76d1a3
Marcus Lindblom <marcus@meridium.se>
2014-07-24 19:11:21 +0200

    Reduce calls to RavenDB - Add static property to hold the if the site has a configuration document - Instead of making a full document load we use a HEAD request and check if the document exists

--- BrickPile.Core/Mvc/BrickPileControllerFactory.cs
+++ BrickPile.Core/Mvc/BrickPileControllerFactory.cs
@@ -1,33 +1,15 @@
-﻿/* Copyright (C) 2011 by Marcus Lindblom
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE. */
-
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
-using BrickPile.Core.Configuration;
+using BrickPile.Core.Extensions;
 using Raven.Client;
 using StructureMap;
 
 namespace BrickPile.Core.Mvc {
     public class BrickPileControllerFactory : DefaultControllerFactory {
+
         private readonly IDocumentStore _store;
         private static bool _hasConfiguration;
+
         /// <summary>
         /// Creates the specified controller by using the specified request context.
         /// </summary>
@@ -40,14 +22,11 @@ namespace BrickPile.Core.Mvc {
         ///   
         /// <exception cref="T:System.ArgumentException">The <paramref name="controllerName"/> parameter is null or empty.</exception>
         public override IController CreateController(RequestContext requestContext, string controllerName) {
-            if (!HasConfiguration()) {
-                requestContext.RouteData.DataTokens["area"] = "ui";
-                requestContext.RouteData.Values["action"] = "index";
-                requestContext.RouteData.Values["controller"] = "setup";
-                return base.CreateController(requestContext, "setup");
-            }
-
-            return base.CreateController(requestContext, controllerName);
+            if (HasConfiguration()) return base.CreateController(requestContext, controllerName);
+            requestContext.RouteData.DataTokens["area"] = "ui";
+            requestContext.RouteData.Values["action"] = "index";
+            requestContext.RouteData.Values["controller"] = "setup";
+            return base.CreateController(requestContext, "setup");
         }
         /// <summary>
         /// Determines whether this instance has configuration.
@@ -56,13 +35,8 @@ namespace BrickPile.Core.Mvc {
         ///   <c>true</c> if this instance has configuration; otherwise, <c>false</c>.
         /// </returns>
         private bool HasConfiguration() {
-            IConfiguration configuration;
-            using (var session = _store.OpenSession())
-            {
-                configuration = session.Load<IConfiguration>("brickpile/configuration");
-            }
-            if(!_hasConfiguration) {                
-                _hasConfiguration = configuration != null;
+            if (!_hasConfiguration) {
+                _hasConfiguration = _store.Exists("brickpile/configuration");
             }
             return _hasConfiguration;
         }
