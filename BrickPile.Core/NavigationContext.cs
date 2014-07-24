c7ee8adbc0beb61fa09c215228a90ce55fbbda49
Marcus Lindblom <marcus@meridium.se>
2014-07-24 18:08:05 +0200

    Add NavigationContext class     - Creating a new intstance of this class will give you the current navigation context from the property CurrentContext

---
+++ BrickPile.Core/NavigationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Routing;
+using BrickPile.Core.Routing;
+
+namespace BrickPile.Core
+{
+    /// <summary>
+    /// Represents the navigation context and contains the current page and it's child nodes, each ancestors and their child nodes.
+    /// The navigation context is used to render the menu and sub menu
+    /// </summary>
+    public class NavigationContext
+    {
+        private readonly RequestContext _requestContext;
+
+        public IPage[] CurrentContext {
+            get {
+                if (!_requestContext.RouteData.DataTokens.ContainsKey(RouteResolver.NavigationContextKey)) {
+                    throw new Exception("The current request context does not contain the current navigation context.");
+                }
+                return _requestContext.RouteData.DataTokens[RouteResolver.NavigationContextKey] as IPage[];
+            }
+        }
+
+        public NavigationContext(RequestContext requestContext) {
+            _requestContext = requestContext;
+        }
+    }
+}
