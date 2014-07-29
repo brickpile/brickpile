f1a905a14a4f44c91dc01a56b43c491ad2bb10ac
Marcus Lindblom <marcus@meridium.se>
2014-07-29 09:41:08 +0200

    Ensure secure by default

--- BrickPile.UI/App_Start/FilterConfig.cs
+++ BrickPile.UI/App_Start/FilterConfig.cs
@@ -5,6 +5,7 @@ namespace BrickPile.Samples {
     public class FilterConfig {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
