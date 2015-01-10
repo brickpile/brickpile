eb1bbc124e37480737bb85f08c13b943c5fc89f7
Marcus Lindblom <marcus@meridium.se>
2015-01-10 19:38:14 +0100

    Add method that swollows type loader exceptions

---
+++ BrickPile.Core/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BrickPile.Core.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        public static IEnumerable<Type> TryGetTypes(this Assembly assm)
+        {
+            try
+            {
+                return assm.GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                return e.Types.Where(t => t != null);
+            }
+        }
+    }
+}
--- BrickPile.UI/App_Start/Startup.Bootstrap.cs
+++ BrickPile.UI/App_Start/Startup.Bootstrap.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using BrickPile.Core;
+using BrickPile.Core.Extensions;
 
 namespace BrickPile.UI
 {
@@ -17,7 +20,7 @@ namespace BrickPile.UI
 
             var locatedBootstrappers =
                 from asm in AppDomain.CurrentDomain.GetAssemblies() // TODO ignore known assemblies like m$ and such
-                from type in asm.GetTypes()
+                from type in asm.TryGetTypes()
                 where bootstrapperInterface.IsAssignableFrom(type)
                 where !type.IsInterface
                 where type != defaultBootstrapper
