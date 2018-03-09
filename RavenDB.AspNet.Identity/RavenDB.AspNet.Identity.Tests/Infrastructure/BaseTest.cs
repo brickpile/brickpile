580a342c63d9036ca56626b182c0d73d9b9476d1
Marcus Lindblom <marcus@meridium.se>
2018-03-09 16:10:08 +0100

    Add missing project

---
+++ RavenDB.AspNet.Identity/RavenDB.AspNet.Identity.Tests/Infrastructure/BaseTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Raven.Client;
+using Raven.Client.Embedded;
+using Raven.Client.Indexes;
+using RavenDB.AspNet.Identity;
+
+namespace RavenDB.AspNet.Identity.Tests
+{
+    public abstract class BaseTest
+    {
+        protected EmbeddableDocumentStore NewDocStore()
+        {
+            var embeddedStore = new EmbeddableDocumentStore
+            {
+                Configuration =
+                {
+                    RunInMemory = true,
+                    RunInUnreliableYetFastModeThatIsNotSuitableForProduction = true
+                }
+            };
+
+            embeddedStore.Initialize();
+
+            new RavenDocumentsByEntityName().Execute(embeddedStore);
+
+            return embeddedStore;
+        }
+
+        protected UserStore<TUser> NewUserStore<TUser>(IDocumentStore docStore) where TUser : IdentityUser
+        {
+            return new UserStore<TUser>(docStore.OpenAsyncSession);
+        }
+
+        protected UserManager<TUser> NewUserManager<TUser>(IDocumentStore docStore) where TUser : IdentityUser
+        {
+            return new UserManager<TUser>(new UserStore<TUser>(docStore.OpenAsyncSession));
+        }
+    }
+}
---
+++ RavenDB.AspNet.Identity/RavenDB.AspNet.Identity.Tests/Models/SimpleAppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RavenDB.AspNet.Identity;
+
+namespace RavenDB.AspNet.Identity.Tests
+{
+    public class SimpleAppUser : IdentityUser
+    {
+    }
+}
